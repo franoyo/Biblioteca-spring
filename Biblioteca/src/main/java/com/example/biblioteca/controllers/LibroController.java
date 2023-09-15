03e9e217710fb2b41d0f4cfa5316d752143cf0e2
franoyo <oyola294@gmail.com>
2023-09-15 10:00:55 -0500

    crud update

--- Biblioteca/src/main/java/com/example/biblioteca/controllers/LibroController.java
+++ Biblioteca/src/main/java/com/example/biblioteca/controllers/LibroController.java
@@ -4,13 +4,11 @@
  */
 package com.example.biblioteca.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.biblioteca.services.impl.LibroService;
 
 /**
  *
@@ -23,15 +21,21 @@ public class LibroController {
     @GetMapping ("/index")
     public String vistaIndex(Model model) {
         return "Index";  }
-    
-          @Autowired
-    private LibroService libroService;
 @GetMapping("/crudLibros")
 public  String vistaCrudLibros(Model model){
-    model.addAttribute("Libro", libroService.findAll());
 return "crudLibros";
 
 }
+@GetMapping("/addLibro")
+public  String vistaAdd(Model model){
+return "formAddLibro";
+
+
+
+}
+
+
+
 
    
     
