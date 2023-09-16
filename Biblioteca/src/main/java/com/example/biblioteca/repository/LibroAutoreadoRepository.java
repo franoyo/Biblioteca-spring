784119f61e85895e35b7edd477a61f12b1895e3f
franoyo <oyola294@gmail.com>
2023-09-16 12:04:03 -0500

    macox

--- Biblioteca/src/main/java/com/example/biblioteca/repository/LibroAutoreadoRepository.java
+++ Biblioteca/src/main/java/com/example/biblioteca/repository/LibroAutoreadoRepository.java
@@ -4,12 +4,6 @@
  */
 package com.example.biblioteca.repository;
 
-import com.example.biblioteca.entity.LibroAutoreado;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.TypedQuery;
-import jakarta.transaction.Transactional;
-import java.util.List;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -18,37 +12,4 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class LibroAutoreadoRepository {
-
-    @PersistenceContext
-    private EntityManager entityM;
-
-    public List<LibroAutoreado> findAll() {
-        TypedQuery<LibroAutoreado> buscar = entityM.createQuery("select l from libro_autoreado l", LibroAutoreado.class);
-        return buscar.getResultList();
-    }
-
-    @Transactional
-    public void guardar(LibroAutoreado libroAutoreado) {
-        entityM.persist(libroAutoreado);
-    }
-
-    @Transactional
-    public LibroAutoreado actualizar(LibroAutoreado libroAutoreado) {
-        entityM.merge(libroAutoreado);
-        return libroAutoreado;
-    }
-
-    @Transactional
-    public LibroAutoreado encontarPorId(long id) {
-        LibroAutoreado libroAutoreadoActual = entityM.find(LibroAutoreado.class, id);
-        return libroAutoreadoActual;
-    }
-
-    @Transactional
-    public void eliminarPorId(long id) {
-        LibroAutoreado eliminarlibroAutoreado = encontarPorId(id);
-        if (eliminarlibroAutoreado != null) {
-            entityM.remove(eliminarlibroAutoreado);
-        }
-    }
 }
--- Biblioteca/src/main/resources/static/js/scriptAlerta.js
+++ Biblioteca/src/main/resources/static/js/scriptAlerta.js
@@ -47,3 +47,30 @@ editButtons.forEach(function(button) {
 function cerrarModal(modalId, modalc) {
   modalc.classList.remove('ver')// Quitar la clase "aparecer" para ocultar el modal
 }
+
+var verButton = document.querySelectorAll('.edit-buttons.see-button');
+// Iterar sobre cada botón obtenido
+verButton.forEach(function(button1) {
+  button1.addEventListener('click', function(event) {
+      // Prevenir el comportamiento predeterminado del evento (en este caso, la acción de navegación de los enlaces)
+    event.preventDefault();
+     // Obtener el atributo "data-id" del botón, que contiene el ID del empleado correspondiente
+    var empleadoId = this.getAttribute('data-id');
+    // Construir el ID del modal único concatenando el ID del empleado con la cadena "modal-editar"
+    var modalId = "modal-ver" + empleadoId;
+    console.log(modalId)
+     // Mostrar el modal seleccionándolo por su ID y agregando la clase CSS "aparecer"
+    var modalc = document.getElementById(modalId);
+    modalc.classList.add("ver") 
+      // Obtener el botón de cierre dentro del modal
+      var closeButton = modalc.querySelector('.botoncito');
+       // Agregar evento de clic al botón de cierre para llamar a la función cerrarModal
+      closeButton.addEventListener('click', function() {
+        cerrarModal(modalId, modalc)// Pasar el ID del modal y el elemento modal como parámetros
+    });
+    });
+});
+// Función para cerrar el modal
+function cerrarModal(modalId, modalc) {
+  modalc.classList.remove('ver')// Quitar la clase "aparecer" para ocultar el modal
+}
